815da8da323d9bc2f9133f98d5473dd6d2a08fe3
Kyle Cheung <cheung.kyle1@gmail.com>
2025-01-07 12:55:03 -0500

    added drizzle orm

---
+++ src/db/index.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { config } from "dotenv";
+import { Pool } from "pg";
+
+config({ path: ".env.local" });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle({ client: pool });
