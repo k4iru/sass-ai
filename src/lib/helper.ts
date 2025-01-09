ac3746a5d5e690fb8501dd85f2c2d986e0c77a79
Kyle Cheung <cheung.kyle1@gmail.com>
2025-01-09 14:45:29 -0500

    added getUser function + simplified imports

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
-import { db } from "@/db";
 import { eq } from "drizzle-orm";
-import { usersTable } from "@/db/schema";
+import { db, usersTable } from "@/db/schema";
+
+type User = typeof usersTable.$inferSelect;
 
 export const response = (success: boolean, message: string, status: number): NextResponse => {
   return NextResponse.json({ success, message }, { status });
@@ -10,4 +11,12 @@ export const response = (success: boolean, message: string, status: number): Nex
 export async function isExistingUser(email: string) {
   const existingUser = await db.select().from(usersTable).where(eq(usersTable.email, email));
   return existingUser.length > 0;
+}
+
+export async function getUser(email: string): Promise<User | null> {
+  const user = await db.select().from(usersTable).where(eq(usersTable.email, email));
+
+  if (!user.length) return null;
+
+  return user[0];
 }
