dc74fd6b56d95d9b33fd7a2608e54784a2cc4557
Kyle Cheung <cheung.kyle1@gmail.com>
2025-01-09 14:43:57 -0500

    create login component

---
+++ src/app/login/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import React, { FormEvent, useState } from "react";
+// import { signupSchema } from "@/lib/validation/signupSchema";
+import { redirect } from "next/navigation";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState<string | null>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  // TODO look up server actions vs restful API. leaning towards just implementing everything as restful api though.
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    // reset set up
+    e.preventDefault();
+    setLoading(true);
+    setMessage(null);
+
+    /*
+    // Handle client validation use zod. Remember to also validate server side as well.
+    const validationResult = signupSchema.safeParse(formData);
+    if (!validationResult.success) {
+      const errors = validationResult.error.errors.map((err) => err.message).join(", ");
+      setMessage(errors);
+      setLoading(false); // Stop loading if validation fails
+      return;
+    } */
+
+    // handle post
+    try {
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      // success!
+      if (!response.ok) {
+        setMessage("Something went wrong.");
+        setLoading(false);
+
+        return;
+      }
+    } catch (error) {
+      setMessage("Error submitting form.");
+      setLoading(false);
+
+      return;
+    }
+
+    // since redirect throws an error this needs to be outside the try catch block.
+    setLoading(false);
+    redirect("/dashboard");
+    return;
+
+    // handle message
+  };
+  return (
+    <div className="signup-form max-w-3xl bg-gray-600 mx-auto my-auto p-8 flex flex-col">
+      <h1 className="text-center pt-6">Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            className="bg-gray-400 rounded-md ml-2 mb-2"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            className="bg-gray-400 rounded-md ml-2 mb-2"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        {message && <p className="text-red-500 text-small mt-2">{message}</p>}
+        <div className="flex flex-row">
+          <button
+            disabled={loading}
+            type="submit"
+            className="border-black border-2 rounded-2xl py-2 px-4 hover:bg-gray-300 mx-auto mt-8">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
