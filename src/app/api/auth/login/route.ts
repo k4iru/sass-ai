47da1f7e5a25d9112b404c5c3091897b5a5f082d
Kyle Cheung <cheung.kyle1@gmail.com>
2025-01-09 14:43:49 -0500

    create login route

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest } from "next/server";
+import { verifyPassword } from "@/lib/auth";
+import { generateAccessToken, generateRefreshToken } from "@/lib/jwt";
+import { isExistingUser, getUser, response } from "@/lib/helper";
+import { db, tokensTable } from "@/db/schema";
+
+export async function POST(req: NextRequest) {
+  const { email, password } = await req.json();
+  const user = await getUser(email);
+
+  // check user exists
+  if (user === null) {
+    return response(false, "Invalid email or password", 401);
+  }
+
+  // check password matches
+  const valid = verifyPassword(password, user.password);
+  if (!valid) return response(false, "Invalid email or password", 401);
+
+  // Authenticated! create tokens and send
+  const accessToken = generateAccessToken(user);
+  console.log(accessToken);
+
+  const refreshToken = generateRefreshToken(user);
+  console.log(refreshToken);
+
+  await db.insert();
+}
