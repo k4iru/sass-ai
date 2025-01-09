a7a0a1e5e4a3fd7f624696b1526174dcbd5cc454
Kyle Cheung <cheung.kyle1@gmail.com>
2025-01-09 14:45:48 -0500

    updated payload for access token generation

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -5,12 +5,12 @@ const JWT_EXPIRY = process.env.JWT_EXPIRY;
 const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
 
 // Generate access token
-export function generateAccessToken(user: { id: string; email: string }) {
-  return jwt.sign({ id: user.id, email: user.email }, `${JWT_SECRET}`, { expiresIn: JWT_EXPIRY });
+export function generateAccessToken(user: { email: string }) {
+  return jwt.sign({ iss: "https://sass-ai", aud: "https://sass-ai", sub: `email=${user.email}` }, `${JWT_SECRET}`, { expiresIn: JWT_EXPIRY });
 }
 
 // Generate refresh token
-export function generateRefreshToken(user: { id: string }) {
+export function generateRefreshToken(user: { id: number }) {
   return jwt.sign({ id: user.id }, `${JWT_SECRET}`, { expiresIn: REFRESH_TOKEN_EXPIRY });
 }
 
