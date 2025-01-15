583addfc56453a2546158f6d99ecb061e59e3a93
Kyle Cheung <cheung.kyle1@gmail.com>
2025-01-14 20:01:17 -0500

    moved authentication check onto layout so that header would be included in checks

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,39 @@
+"use client";
+
 import Header from "@/components/Header";
-import React from "react";
+import { useAuth } from "@/context/AuthContext";
+import React, { useEffect, useState } from "react";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const [authenticated, setAuthenticated] = useState(false);
+  const { accessToken, refreshAccessToken } = useAuth();
+
+  useEffect(() => {
+    // 1. check if accesstoken exists and is valid
+
+    // 2. if valid but expired try and refresh
+
+    // 3.
+
+    const fetchData = async () => {
+      try {
+        refreshAccessToken();
+      } catch (err) {
+        console.log(`error getting refresh token` + (err instanceof Error ? err.message : "unknown"));
+      }
+    };
+    fetchData();
+
+    if (accessToken !== null) setAuthenticated(true);
+    else setAuthenticated(false);
+  }, [accessToken]);
+
+  if (!authenticated)
+    return (
+      <div>
+        <p>not authenticated</p>
+      </div>
+    );
   return (
     <div className="flex flex-col flex-1 h-screen">
       <Header />
