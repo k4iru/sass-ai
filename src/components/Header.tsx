da25207bff4859fe4b5ea41ebdc3abcdeabe6a32
Kyle Cheung <cheung.kyle1@gmail.com>
2025-01-14 20:01:27 -0500

    added sign out button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,13 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { FilePlus2 } from "lucide-react";
+import { useAuth } from "@/context/AuthContext";
 
 function Header() {
+  const { logout } = useAuth();
+  const handleClick = () => {
+    logout();
+  };
   return (
     <div className="flex justify-between bg-white shadow-sm p-5 border-b">
       <Link
@@ -33,6 +38,12 @@ function Header() {
             <Link href="/dashboard/upload">
               <FilePlus2 className="text-indigo-600" />
             </Link>
+          </Button>
+
+          <Button
+            asChild
+            onClick={handleClick}>
+            <p>Logout</p>
           </Button>
 
           {/* User Button */}
