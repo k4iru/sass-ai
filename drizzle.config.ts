b41b38941be5cca61dca99bf893d38028f43c0bb
Kyle Cheung <cheung.kyle1@gmail.com>
2025-04-03 20:28:45 -0400

    changed migration folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 config({ path: ".env.local" });
 
 export default defineConfig({
-  out: "./drizzle",
+  out: "./drizzle/migrations",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
