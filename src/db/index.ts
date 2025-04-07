f8710f7bcde9958ac54028679fd8acc99bb6353d
Kyle Cheung <cheung.kyle1@gmail.com>
2025-04-07 00:34:04 -0400

    added client for open tcp connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { config } from "dotenv";
-import { Pool, Client } from "pg";
+import pg from "pg";
+const { Pool, Client } = pg;
 import * as schema from "./schema";
 
 config({ path: ".env.local" });
@@ -14,17 +15,17 @@ const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
 
-client.connect().then(() => {
-  console.log("Connected to PostgreSQL for LISTEN/NOTIFY");
-});
+// client.connect().then(() => {
+//   console.log("Connected to PostgreSQL for LISTEN/NOTIFY");
+// });
 
-// Listen for PostgreSQL NOTIFY events
-client.query("LISTEN new_message");
+// // Listen for PostgreSQL NOTIFY events
+// client.query("LISTEN new_message");
 
-client.on("notification", (msg) => {
-  console.log("New message notification received:", msg.payload);
-  // You will need to broadcast this update via WebSockets
-});
+// client.on("notification", (msg) => {
+//   console.log("New message notification received:", msg.payload);
+//   // You will need to broadcast this update via WebSockets
+// });
 
 export const db = drizzle({ client: pool }); // drizzle for updating / deleting / inserting data
 export { schema, client }; // client for LISTEN/NOTIFY
