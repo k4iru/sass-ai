e1112b60c394b80dce0488765655bb335e160b25
Kyle Cheung <cheung.kyle1@gmail.com>
2025-04-19 00:28:03 -0400

    fixed linting + formatting

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,40 +8,47 @@ import { getRefreshToken } from "./helper";
 
 // Hash password
 export async function hashPassword(password: string): Promise<string> {
-  try {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    return hashedPassword;
-  } catch (err) {
-    throw new Error("Error hashing password: " + (err instanceof Error ? err.message : "Unknown error"));
-  }
+	try {
+		const saltRounds = 10;
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
+		return hashedPassword;
+	} catch (err) {
+		throw new Error(
+			`Error hashing password: ${err instanceof Error ? err.message : "Unknown error"}`,
+		);
+	}
 }
 
 // Compare password
-export async function verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
-  try {
-    const isMatch = await bcrypt.compare(password, hashedPassword);
-    return isMatch;
-  } catch (err) {
-    throw new Error("Error comparing password: " + (err instanceof Error ? err.message : "Unknown error"));
-  }
+export async function verifyPassword(
+	password: string,
+	hashedPassword: string,
+): Promise<boolean> {
+	try {
+		const isMatch = await bcrypt.compare(password, hashedPassword);
+		return isMatch;
+	} catch (err) {
+		throw new Error(
+			`Error comparing password: ${err instanceof Error ? err.message : "Unknown error"}`,
+		);
+	}
 }
 
 export async function authenticate(): Promise<void> {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-  const refreshToken = cookieStore.get("refreshToken")?.value;
+	const cookieStore = await cookies();
+	const accessToken = cookieStore.get("accessToken")?.value;
+	const refreshToken = cookieStore.get("refreshToken")?.value;
 
-  if (!accessToken || !refreshToken) throw new Error("Unauthorized");
+	if (!accessToken || !refreshToken) throw new Error("Unauthorized");
 
-  try {
-    const verified = await userVerified(accessToken);
-    // add additional verification for checking refresh token. since this allows anyone with any access token to run.
+	try {
+		const verified = await userVerified(accessToken);
+		// add additional verification for checking refresh token. since this allows anyone with any access token to run.
 
-    const refreshTokenExists = getRefreshToken(refreshToken);
+		const refreshTokenExists = getRefreshToken(refreshToken);
 
-    if (!verified || !refreshTokenExists) throw new Error("Unauthorized");
-  } catch (err) {
-    console.log(err);
-  }
+		if (!verified || !refreshTokenExists) throw new Error("Unauthorized");
+	} catch (err) {
+		console.log(err);
+	}
 }
