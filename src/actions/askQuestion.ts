f35c247fcfa2354f886f37bfc1f16ec073b2997c
Kyle Cheung <cheung.kyle1@gmail.com>
2025-04-19 00:56:59 -0400

    initial logic for generating ai response

--- src/actions/askQuestion.ts
+++ src/actions/askQuestion.ts
@@ -2,20 +2,39 @@
 
 import { useAuth } from "@/context/AuthContext";
 import { authenticate } from "@/lib/auth";
-import { Message } from "@/types/types";
+import type { Message } from "@/types/types";
 import { insertMessage } from "@/lib/helper";
+
+const FREE_LIMT = 3;
+const PRO_LIMIT = 100;
 
 // implemented as server action
 export async function askQuestion(message: Message) {
-  authenticate();
-  const { user } = useAuth();
+	// verify user authentication from cookies
+	authenticate();
+	const { user } = useAuth();
 
-  if (!user) {
-    throw new Error("User not authenticated");
-  }
+	if (!user) {
+		throw new Error("User not authenticated");
+	}
 
-  // insert into db
-  const success = await insertMessage(message);
+	// generate AI reply
+	const reply = await generateLangchainCompletion(message.content);
 
-  // send message to api for response and insert into db
+	const aiMessage: Message = {
+		role: "ai",
+		chatId: message.chatId,
+		userId: message.userId,
+		content: reply,
+		createdAt: new Date(),
+	};
+
+	// insert into db
+	const success = await insertMessage(aiMessage);
+
+	if (success) {
+		return { success: true, message: null };
+	}
+
+	// send message to api for response and insert into db
 }
