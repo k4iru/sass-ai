ca5bcd0f23dd189a71fa7d1978fd5808b26fd8bc
Kyle Cheung <cheung.kyle1@gmail.com>
2025-05-08 18:47:59 -0400

    added light dark theme provider

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import type { Theme } from "@/types/types";
+
+const defaultTheme: Theme = {
+	theme: "light",
+	toggleTheme: () => {},
+};
+
+const ThemeContext = createContext<Theme>(defaultTheme);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+	const [theme, setTheme] = useState<"light" | "dark">("light");
+
+	const toggleTheme = () => {
+		const newTheme = theme === "light" ? "dark" : "light";
+		setTheme(newTheme);
+		document.documentElement.classList.toggle("dark", newTheme === "dark");
+		localStorage.setItem("theme", newTheme); // Save to localStorage
+	};
+
+	useEffect(() => {
+		// on load check for saved theme
+		const savedTheme = localStorage.getItem("theme");
+		if (savedTheme === "dark" || savedTheme === "light") {
+			setTheme(savedTheme);
+			document.documentElement.classList.toggle("dark", savedTheme === "dark");
+		} else {
+			// default to system preference
+			const prefersDark = window.matchMedia(
+				"(prefers-color-scheme: dark)",
+			).matches;
+			setTheme(prefersDark ? "dark" : "light");
+			document.documentElement.classList.toggle("dark", prefersDark);
+			localStorage.setItem("theme", prefersDark ? "dark" : "light"); // Save to localStorage
+		}
+	}, []);
+
+	return (
+		<ThemeContext.Provider value={{ theme, toggleTheme }}>
+			{children}
+		</ThemeContext.Provider>
+	);
+};
+
+// hook
+export const useTheme = () => useContext(ThemeContext);
--- src/types/types.ts
+++ src/types/types.ts
@@ -10,3 +10,8 @@ export type MessageResponse = {
 	success: boolean;
 	message: string;
 };
+
+export type Theme = {
+	theme: "light" | "dark";
+	toggleTheme: () => void;
+};
