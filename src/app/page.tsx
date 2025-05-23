69fb6d7cb52b66dfb8ad9dc8d5b99883f0c89b64
Kyle Cheung <cheung.kyle1@gmail.com>
2025-05-23 00:48:14 -0400

    removed unused page elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { BrainCogIcon, EyeIcon, GlobeIcon, MonitorSmartphoneIcon, ServerCogIcon, ZapIcon } from "lucide-react";
+import {
+	BrainCogIcon,
+	EyeIcon,
+	GlobeIcon,
+	MonitorSmartphoneIcon,
+	ServerCogIcon,
+	ZapIcon,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-
-const features = [
-  {
-    name: "Store your PDF Documents",
-    description: "Keep all your important PDF files securely and easily accessible anytime",
-    icon: GlobeIcon,
-  },
-  {
-    name: "Blazing Fast Responses",
-    description: "Experience lightning fast answers",
-    icon: ZapIcon,
-  },
-  {
-    name: "Chat Memorisation",
-    description: "Intelligent chatbot remembers",
-    icon: BrainCogIcon,
-  },
-  {
-    name: "Interactive PDF Viewer",
-    description: "Engage with pdfs like never before",
-    icon: EyeIcon,
-  },
-  {
-    name: "Cloud Backup",
-    description: "Rest assured knowing documents are safely backed up on the cloud",
-    icon: ServerCogIcon,
-  },
-  {
-    name: "Responsive Across Devices",
-    description: "Use on any advice",
-    icon: MonitorSmartphoneIcon,
-  },
-];
+import { Header } from "@/components/Header/Header";
 
 export default function Home() {
-  return (
-    <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to-indigo-600">
-      <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
-        <div className="flex flex-col justify-center items-center mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w--2xl sm:text-center">
-            <h2 className="text-base font-semibold leading-7 text-indigo-600">Your Interactive Document Companion</h2>
-            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">Transform Your PDFS into interactive Conversations</p>
-            <p>
-              Introducting <span className="font-bold text-indigo-600">Chat with PDF.</span>
-              <br />
-              <br />
-              <span className="font-bold">dynamic conversations</span>, enhancing productivity
-            </p>
-          </div>
-          <Button
-            asChild
-            className="mt-10">
-            <Link href="/dashboard">Get Started</Link>
-          </Button>
-        </div>
-        <div className="relative overflow-hidden pt-16">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <Image
-              alt="App screenshot"
-              src="https://i.imgur.com/VciRSTI.jpeg"
-              width={2432}
-              height={1442}
-              className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
-            />
-            <div
-              aria-hidden="true"
-              className="relative">
-              <div className="absolute bottom-0 bg-gradient-to-t -inset-x-32 from-white/95 pt-[5%]"></div>
-            </div>
-          </div>
-        </div>
-        <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
-          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
-            {features.map((feature) => (
-              <div
-                className="relative pl-9"
-                key={feature.name}>
-                <dt className="inline font-semibold text-gray-900">
-                  <feature.icon
-                    aria-hidden="true"
-                    className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
-                  />
-                </dt>
-                <dd>{feature.description}</dd>
-              </div>
-            ))}
-          </dl>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<>
+			<Header />
+			<main className="">
+				<p>Main content</p>
+			</main>
+		</>
+	);
 }
