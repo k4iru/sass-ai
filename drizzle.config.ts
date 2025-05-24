817ebaf7bcc41dc51a6b78c086e8c41c6a714de8
Kyle Cheung <cheung.kyle1@gmail.com>
2025-05-24 18:11:02 -0400

    fixed non null assertion

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,16 @@ import { config } from "dotenv";
 
 config({ path: ".env.local" });
 
+const { DATABASE_URL } = process.env;
+if (!DATABASE_URL) {
+	throw new Error("DATABASE_URL is not defined in .env.local");
+}
+
 export default defineConfig({
-  out: "./drizzle/migrations",
-  schema: "./src/db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
+	out: "./drizzle/migrations",
+	schema: "./src/db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: DATABASE_URL,
+	},
 });
