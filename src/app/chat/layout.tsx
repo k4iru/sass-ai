e9497d476a88665f83c534f91000b30a3b09dbb0
Kyle Cheung <cheung.kyle1@gmail.com>
2025-05-31 11:22:00 -0400

    added main layout for chat section

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,10 +1,16 @@
 import { Sidebar } from "@/components/Sidebar/Sidebar";
+import { Header } from "@/components/Header/Header";
+import styles from "./layout.module.scss";
 const ChatLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
-			<Sidebar />
+			{/* swap this header for a chat specific header later to keep pop open menu */}
+			<Header />
+			<div className={styles.layout}>
+				<Sidebar />
 
-			<main>{children}</main>
+				<main>{children}</main>
+			</div>
 		</>
 	);
 };
