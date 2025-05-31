0f9a7eb6ad95a266c713082bee6bd293960ab54d
Kyle Cheung <cheung.kyle1@gmail.com>
2025-05-31 11:23:08 -0400

    initial sidebar functionality

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,44 @@
+"use client";
+import { useState } from "react";
+import clsx from "clsx";
 import styles from "./Sidebar.module.scss";
+import { ArrowLeftFromLine, ArrowRightFromLineIcon } from "lucide-react";
 
 export const Sidebar = () => {
+	const [isOpen, setIsOpen] = useState(true);
+
+	const handleToggleSidebar = () => {
+		setIsOpen(!isOpen);
+	};
 	return (
-		<aside className={styles.sidebar}>
-			<h1>sidebar</h1>
+		<aside className={clsx(styles.sidebar, isOpen && styles.open)}>
+			<div className={styles.topbar}>
+				<input
+					type="text"
+					placeholder="Search..."
+					className={styles.searchInput}
+				/>
+				<button
+					type="button"
+					className={styles.closeButton}
+					onClick={handleToggleSidebar}
+				>
+					<ArrowLeftFromLine
+						className={styles.closeIcon}
+						stroke="currentColor"
+					/>
+				</button>
+				<button
+					type="button"
+					className={styles.openButton}
+					onClick={handleToggleSidebar}
+				>
+					<ArrowRightFromLineIcon
+						className={styles.openIcon}
+						stroke="currentColor"
+					/>
+				</button>
+			</div>
 		</aside>
 	);
 };
