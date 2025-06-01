f96ffd3c4623aa9ffb92f007b1d63707e7c37774
Kyle Cheung <cheung.kyle1@gmail.com>
2025-05-31 23:32:46 -0400

    updated chatbox logic + styles

---
+++ src/components/Chatbox/Chatbox.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useCallback, useEffect, useRef } from "react";
+import styles from "@/components/Chatbox/Chatbox.module.scss";
+import { FilePlus, ArrowBigRight } from "lucide-react";
+
+export const Chatbox = () => {
+	const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+	const resizeTextarea = useCallback(() => {
+		const el = textareaRef.current;
+		if (!el) return;
+		el.style.height = "auto";
+
+		const maxHeight = 160;
+		el.style.height = `${Math.min(el.scrollHeight, maxHeight)}px`;
+	}, []);
+
+	const sendMessage = () => {
+		console.log("Message sent");
+	};
+
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+		if (e.key === "Enter" && !e.shiftKey) {
+			e.preventDefault(); // prevent newline
+			sendMessage();
+		}
+	};
+
+	useEffect(() => {
+		resizeTextarea();
+	}, [resizeTextarea]);
+
+	return (
+		<div className={styles.chatbox}>
+			<div className={styles.input}>
+				<textarea
+					aria-label="Chat input"
+					placeholder="Type your message here..."
+					ref={textareaRef}
+					className={styles.textarea}
+					onInput={resizeTextarea}
+					onKeyDown={handleKeyDown}
+					rows={1}
+				/>
+			</div>
+			<div className={styles.bottom}>
+				<button type="button" className={styles.fileButton}>
+					<FilePlus className={styles.fileIcon} />
+				</button>
+				<span className={styles.spacer} />
+				<div className={styles.modelSelector}>
+					<label htmlFor="model" className={styles.modelLabel}>
+						Model:
+					</label>
+					<select name="model" className={styles.modelSelect} />
+				</div>
+				<button type="button" className={styles.sendButton}>
+					<ArrowBigRight className={styles.sendIcon} />
+				</button>
+			</div>
+		</div>
+	);
+};
