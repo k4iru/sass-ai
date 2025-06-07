cd3a45e152f2351a47406beb3fa7d746a4698a71
Kyle Cheung <cheung.kyle1@gmail.com>
2025-06-06 23:27:56 -0400

    added component / padding for specific chat page

---
+++ src/app/chat/[chatId]/layout.tsx
@@ -0,0 +1,6 @@
+import styles from "./layout.module.scss";
+const chatIdLayout = ({ children }: { children: React.ReactNode }) => {
+	return <>{children}</>;
+};
+
+export default chatIdLayout;
--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,7 +1,14 @@
 const ChatPage = () => {
-  return (
-    <p>chat with specific id page</p>
-  );
-}
+	return (
+		<>
+			<p>chat with specific id page</p>
+			<ul>
+				{Array.from({ length: 100 }, (_, i) => (
+					<li key={i}>Item {i + 1}</li>
+				))}
+			</ul>
+		</>
+	);
+};
 
 export default ChatPage;
