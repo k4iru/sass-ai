cdb05f015c5fbf9ade23e6694473a23dbd8a9349
Kyle Cheung <cheung.kyle1@gmail.com>
2025-06-08 18:07:07 -0400

    removing deprecated files

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-//TODO REMOVE tailwind
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
