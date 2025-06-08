aa7859a183e7c3f20741d3a4688da40663dc6fb7
Kyle Cheung <cheung.kyle1@gmail.com>
2025-06-08 18:07:28 -0400

    removing boilerplate

--- src/app/chat/[chatId]/layout.tsx
+++ src/app/chat/[chatId]/layout.tsx
@@ -1,5 +1,7 @@
 import styles from "./layout.module.scss";
 const chatIdLayout = ({ children }: { children: React.ReactNode }) => {
+	// on load. Need to check if the chatId is valid and also if user has access to it.
+
 	return <>{children}</>;
 };
 
--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,12 +1,7 @@
 const ChatPage = () => {
 	return (
 		<>
-			<p>chat with specific id page</p>
-			<ul>
-				{Array.from({ length: 100 }, (_, i) => (
-					<li key={i}>Item {i + 1}</li>
-				))}
-			</ul>
+			<p>test</p>
 		</>
 	);
 };
