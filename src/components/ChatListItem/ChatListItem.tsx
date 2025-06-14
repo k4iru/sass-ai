50af42fb27b3f111bac08a365704f5d48ca3fda6
Kyle Cheung <cheung.kyle1@gmail.com>
2025-06-14 13:00:32 -0400

    added chat list item as a component

---
+++ src/components/ChatListItem/ChatListItem.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { createPortal } from "react-dom";
+import { useState, useEffect, useRef } from "react";
+import styles from "./ChatListItem.module.scss";
+import Link from "next/link";
+import { MoreVertical } from "lucide-react";
+
+type ChatListItemProps = {
+	id: string;
+	title: string;
+	isMenuOpen: boolean;
+	onCloseMenu: () => void;
+	onToggleMenu: (id: string) => void;
+	onRename: () => void;
+	onDelete: () => void;
+};
+export const ChatListItem = ({
+	id,
+	title,
+	isMenuOpen,
+	onCloseMenu,
+	onToggleMenu,
+	onRename,
+	onDelete,
+}: ChatListItemProps) => {
+	const itemRef = useRef<HTMLLIElement>(null);
+	const popupRef = useRef<HTMLDivElement>(null);
+	const [menuPosition, setMenuPosition] = useState({ top: 0, left: 0 });
+
+	useEffect(() => {
+		if (isMenuOpen && itemRef.current) {
+			const rect = itemRef.current.getBoundingClientRect();
+			setMenuPosition({
+				top: rect.top + window.scrollY,
+				left: rect.right, // adjust based on popup width
+			});
+		}
+		const handleClickOutside = (e: MouseEvent) => {
+			if (
+				isMenuOpen &&
+				itemRef.current &&
+				!itemRef.current.contains(e.target as Node) &&
+				!popupRef.current?.contains(e.target as Node)
+			) {
+				onCloseMenu(); // close this menu
+			}
+		};
+
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [isMenuOpen, onCloseMenu]);
+
+	return (
+		<li className={styles.item} ref={itemRef}>
+			<Link href={`/chat/${id}`}>
+				<span className={styles.chatName}>{title}</span>
+			</Link>
+			<span className={styles.spacer} />
+
+			<button
+				type="button"
+				className={styles.optionsButton}
+				onClick={() => onToggleMenu(id)}
+			>
+				<MoreVertical className={styles.optionsIcon} />
+			</button>
+			{isMenuOpen &&
+				createPortal(
+					<div className={styles.menuPopup} style={menuPosition} ref={popupRef}>
+						<button
+							type="button"
+							className={styles.menuItemRename}
+							onClick={() => console.log("this is a test")}
+						>
+							Rename
+						</button>
+						<button
+							type="button"
+							className={styles.menuItemDelete}
+							onClick={() => onDelete()}
+						>
+							Delete
+						</button>
+					</div>,
+					document.body,
+				)}
+		</li>
+	);
+};
