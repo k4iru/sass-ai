8aedb99ff925e0b67ab963ad3017f1af6b8126c6
Kyle Cheung <cheung.kyle1@gmail.com>
2025-07-01 23:31:22 -0400

    added persistence

--- src/lib/langchain/llmHandler.ts
+++ src/lib/langchain/llmHandler.ts
@@ -2,8 +2,14 @@
 
 import type { Message } from "@/types/types";
 import type { BaseChatModel } from "@langchain/core/language_models/chat_models";
-import type { AIMessageChunk } from "@langchain/core/messages";
-import { HumanMessage } from "@langchain/core/messages";
+import type { AIMessageChunk, BaseMessage } from "@langchain/core/messages";
+import {
+	HumanMessage,
+	AIMessage,
+	SystemMessage,
+} from "@langchain/core/messages";
+import { getMessages } from "../helper";
+import { messages } from "@/db/schema";
 
 const askQuestion = async function* (
 	message: Message,
@@ -12,9 +18,26 @@ const askQuestion = async function* (
 	const { role, chatId, userId, content } = message;
 	const config = { configurable: { thread_id: chatId } };
 
+	// get messages from database
+	const messageHistory = await getMessages(userId, chatId);
+
+	// map to BaseMessageLike
+	const context = messageHistory.map((msg) => {
+		if (msg.role === "human") {
+			return new HumanMessage(msg.content);
+		}
+		if (msg.role === "ai") {
+			return new AIMessage(msg.content);
+		}
+		return new SystemMessage(msg.content);
+	});
+
+	// add question
+	context.push(new HumanMessage(content));
+
 	// const output = await app.invoke({ messages: content }, config);
 
-	const stream = await chatProvider.stream([new HumanMessage(content)], config);
+	const stream = await chatProvider.stream(context, config);
 	const chunks = [];
 	for await (const chunk of stream) {
 		chunks.push(chunk);
