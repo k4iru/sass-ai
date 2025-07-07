ddc9cccbba2d49399609345f8ef0e0d15bef657c
Kyle Cheung <cheung.kyle1@gmail.com>
2025-07-07 11:58:06 -0600

    added chatcontext lru cache

--- src/lib/langchain/llmHandler.ts
+++ src/lib/langchain/llmHandler.ts
@@ -6,7 +6,11 @@ import type {
 	BaseChatModel,
 	BaseChatModelCallOptions,
 } from "@langchain/core/language_models/chat_models";
-import type { AIMessageChunk, BaseMessageLike } from "@langchain/core/messages";
+import type {
+	AIMessageChunk,
+	BaseMessageLike,
+	BaseMessage,
+} from "@langchain/core/messages";
 import {
 	HumanMessage,
 	AIMessage,
@@ -21,8 +25,18 @@ import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { StateGraph, END } from "@langchain/langgraph";
 import type { Runnable } from "@langchain/core/runnables";
 import type { BaseLanguageModelInput } from "@langchain/core/language_models/base";
+import { LRUCache } from "lru-cache";
+import { get_encoding, type TiktokenEncoding } from "@dqbd/tiktoken";
 
 // https://langchain-ai.github.io/langgraphjs/how-tos/stream-tokens
+
+const TOKEN_LIMIT = 8192; // 8k tokens
+
+const chatCache = new LRUCache<string, BaseMessageLike[]>({
+	max: 100,
+	ttl: 1000 * 60 * 60,
+	allowStale: false,
+});
 
 const StateAnnotation = Annotation.Root({
 	messages: Annotation<BaseMessageLike[]>({
@@ -94,13 +108,30 @@ const handleWorkFlow = (
 	return { agent };
 };
 
+// create a function to approximate numbers of tokens. useful for send limits.
+const approximateTokens = (
+	context: Message[],
+	encodingName = "cl100k_base",
+) => {
+	const enc = get_encoding(encodingName as TiktokenEncoding);
+	let tokenCount = 0;
+	for (const message of context) {
+		const tokens = enc.encode(message.content as string);
+		tokenCount += 4;
+		tokenCount += tokens.length;
+		tokenCount += message.role.length;
+		tokenCount += 2;
+	}
+
+	return tokenCount;
+};
+
 //TODO add persistence still to chat
 const askQuestion = async function* (
 	message: Message,
 	chatProvider: BaseChatModel,
 ): AsyncGenerator<AIMessageChunk> {
 	const { role, chatId, userId, content } = message;
-	const config = { configurable: { thread_id: chatId } };
 
 	const boundChatProvider =
 		typeof chatProvider.bindTools === "function"
@@ -113,8 +144,36 @@ const askQuestion = async function* (
 
 	const { agent } = handleWorkFlow(boundChatProvider);
 
-	// get messages from database
+	// get messages from database. potentially change this to be more efficient.
+	// get messages up to token limit
+
+	// returns list of Message objects
 	const messageHistory = await getMessages(userId, chatId);
+
+	const chatKey = `${userId}-${chatId}`;
+	const chatContext = chatCache.get(chatKey);
+	let tokenCount = -1;
+
+	if (!chatContext) {
+		// fetch from database.
+		// fetch messages up to token limit than summarize it.
+		// if first message approximate first message. else try and grab context from db.
+		if (messageHistory.length === 0) {
+			tokenCount = approximateTokens([message]);
+		} else {
+			tokenCount = approximateTokens(messageHistory);
+		}
+	}
+
+	console.log(tokenCount);
+
+	if (chatCache.has(chatKey)) {
+		console.log("Using cached chat context for", chatKey);
+	}
+	// schema for context.
+	// have a system message at the start with instructions plus summary of chat if too many messages / tokens used. System message + summary should be less than 250 tokens.
+	// can fill rest of token limit with last x messages. as new messages are added. remove the oldest messages and resummarize chat.
+	// if chatCache has the chatId, use that instead of fetching from database.
 
 	// map to BaseMessageLike
 	const context = messageHistory.map((msg) => {
@@ -135,19 +194,30 @@ const askQuestion = async function* (
 	//const stream = await chatProvider.stream(context, config);
 	const stream = await agent.stream(
 		{
-			messages: [{ role: "user", content: content }],
+			messages: context,
 		},
-		{ streamMode: "messages" },
+		{ streamMode: "messages", configurable: { thread_id: chatId } },
 	);
 	const chunks = [];
 	for await (const [chunk, _metadata] of stream) {
 		if (isAIMessageChunk(chunk as AIMessageChunk) && chunk.content.length > 0) {
-			console.log("chunk", chunk);
 			chunks.push(chunk);
 
 			yield chunk as AIMessageChunk;
 		}
+
+		if ((chunk as AIMessageChunk).usage_metadata) {
+			const inputTokens =
+				(chunk as AIMessageChunk).usage_metadata?.input_tokens || 0; // human query
+			const outputTokens =
+				(chunk as AIMessageChunk).usage_metadata?.output_tokens || 0; // ai response
+
+			// create batch to push both to db.
+			// order is important. human message first, then ai message.
+		}
 	}
+
+	console.log(chunks);
 };
 
 export { askQuestion };
