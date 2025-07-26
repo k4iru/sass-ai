9eaee90469adab78ef14234340357e9f45f07bc8
Kyle Cheung <cheung.kyle1@gmail.com>
2025-07-26 18:58:13 -0400

    added custom input component

---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,23 @@
+import styles from "./Input.module.scss";
+import clsx from "clsx";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	className?: string;
+	// You can add other custom props here if needed
+}
+
+export const Input: React.FC<InputProps> = ({
+	className,
+	type,
+	value,
+	...props
+}: InputProps) => {
+	return (
+		<input
+			type={type}
+			className={clsx(styles.input, className)}
+			{...props}
+			value={value}
+		/>
+	);
+};
