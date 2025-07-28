3a903b28a40cc1f97da17c627eaf90da7ad7d376
Kyle Cheung <cheung.kyle1@gmail.com>
2025-07-28 00:25:48 -0400

    added initial login styling

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,7 @@
+import styles from "./layout.module.scss";
+
+const LoginLayout = ({ children }: { children: React.ReactNode }) => {
+	return <div className={styles.layout}>{children}</div>;
+};
+
+export default LoginLayout;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,25 @@
 "use client";
 
 import type React from "react";
-import { type FormEvent, useState } from "react";
-// import { signupSchema } from "@/lib/validation/signupSchema";
+import { type FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
+import styles from "./login.module.scss";
 
 function Login() {
 	const router = useRouter();
-	const { login } = useAuth();
+	const { login, user } = useAuth();
 	const [formData, setFormData] = useState({
 		email: "",
 		password: "",
 	});
 	const [loading, setLoading] = useState(false);
 	const [message, setMessage] = useState<string | null>(null);
+
+	useEffect(() => {
+		// user already logged in
+		if (user) router.push("/chat");
+	});
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target;
@@ -40,19 +45,18 @@ function Login() {
 
 		// since redirect throws an error this needs to be outside the try catch block.
 		setLoading(false);
-		router.replace("/chat");
+		router.push("/chat");
 		return;
 
 		// handle message
 	};
 	return (
-		<div className="signup-form max-w-3xl bg-gray-600 mx-auto my-auto p-8 flex flex-col">
-			<h1 className="text-center pt-6">Login</h1>
+		<div className={styles.wrapper}>
+			<h1 className={styles.header}>Welcome Back</h1>
 			<form onSubmit={handleSubmit}>
-				<div>
-					<label htmlFor="email">Email:</label>
+				<div className={styles.input}>
+					<label htmlFor="email">Email</label>
 					<input
-						className="bg-gray-400 rounded-md ml-2 mb-2"
 						type="email"
 						name="email"
 						value={formData.email}
@@ -60,10 +64,9 @@ function Login() {
 						required
 					/>
 				</div>
-				<div>
-					<label htmlFor="password">Password:</label>
+				<div className={styles.input}>
+					<label htmlFor="password">Password</label>
 					<input
-						className="bg-gray-400 rounded-md ml-2 mb-2"
 						type="password"
 						name="password"
 						value={formData.password}
@@ -72,12 +75,12 @@ function Login() {
 					/>
 				</div>
 
-				{message && <p className="text-red-500 text-small mt-2">{message}</p>}
-				<div className="flex flex-row">
+				{message && <p className="">{message}</p>}
+				<div className={styles.loginWrapper}>
 					<button
 						disabled={loading}
 						type="submit"
-						className="border-black border-2 rounded-2xl py-2 px-4 hover:bg-gray-300 mx-auto mt-8"
+						className={styles.loginButton}
 					>
 						Login
 					</button>
