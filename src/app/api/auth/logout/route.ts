c08705c8750a9fc935cc50df5bee89c3f1d61bf9
Kyle Cheung <cheung.kyle1@gmail.com>
2025-08-16 00:46:53 -0400

    formatting

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,22 +1,40 @@
+import { type NextRequest, NextResponse } from "next/server";
 import { deleteRefreshToken } from "@/lib/helper";
-import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  try {
-    const refreshToken = req.cookies.get("refreshToken")?.value;
-    if (refreshToken === undefined) return NextResponse.json({ error: "Invalid cookies" }, { status: 400 });
+	try {
+		const refreshToken = req.cookies.get("refreshToken")?.value;
+		if (refreshToken === undefined)
+			return NextResponse.json({ error: "Invalid cookies" }, { status: 400 });
 
-    // deletes session id from database
-    const success = await deleteRefreshToken(refreshToken);
+		// deletes session id from database
+		const success = await deleteRefreshToken(refreshToken);
 
-    if (!success) NextResponse.json({ error: "Failed to delete session" }, { status: 500 });
+		if (!success)
+			NextResponse.json({ error: "Failed to delete session" }, { status: 500 });
 
-    const response = NextResponse.json({ message: "Logged out" }, { status: 200 });
-    response.cookies.set("refreshToken", "", { maxAge: 0, path: "/", httpOnly: true, secure: true });
-    response.cookies.set("accessToken", "", { maxAge: 0, path: "/", httpOnly: true, secure: true });
+		const response = NextResponse.json(
+			{ message: "Logged out" },
+			{ status: 200 },
+		);
+		response.cookies.set("refreshToken", "", {
+			maxAge: 0,
+			path: "/",
+			httpOnly: true,
+			secure: true,
+		});
+		response.cookies.set("accessToken", "", {
+			maxAge: 0,
+			path: "/",
+			httpOnly: true,
+			secure: true,
+		});
 
-    return response;
-  } catch (err) {
-    console.error("failed to logout properly", err instanceof Error ? err.message : "unknown error");
-  }
+		return response;
+	} catch (err) {
+		console.error(
+			"failed to logout properly",
+			err instanceof Error ? err.message : "unknown error",
+		);
+	}
 }
