69bc09903a4d83f9150c231f83e149d7cf644d12
Kyle Cheung <cheung.kyle1@gmail.com>
2025-11-09 20:32:48 -0500

    Create schema.ts

---
+++ src/lib/env/schema.ts
@@ -0,0 +1,38 @@
+// src/lib/env/schema.ts
+import { z } from "zod";
+
+// server environment variables
+export const serverSchema = z.object({
+	ENCRYPTION_KEY: z.string().min(64).max(64),
+	REFRESH_TOKEN_EXPIRY: z.string().min(1),
+	DATABASE_URL: z.string().url(),
+	AWS_ACCESS_KEY_ID: z.string().min(8),
+	AWS_SECRET_ACCESS_KEY: z.string().min(16),
+	AWS_REGION: z.string().min(1),
+	AWS_BUCKET_NAME: z.string().min(1),
+	PINECONE_API_KEY: z.string().min(1),
+	OPENAI_API_KEY: z.string().min(1),
+	WS_PORT: z.string().min(1),
+	GOOGLE_CLIENT_ID: z.string().min(1),
+	GOOGLE_CLIENT_SECRET: z.string().min(1),
+	GOOGLE_REDIRECT_URI: z.string().min(1),
+	RESEND_API_KEY: z.string().min(1),
+});
+
+// middleware accessible
+export const edgeSchema = z.object({
+	JWT_SECRET: z.string().min(32),
+	JWT_EXPIRY: z.string().min(1),
+	JWT_AUD: z.string().min(1),
+	JWT_ISS: z.string().min(1),
+});
+
+// client. make sure to use NEXT_PUBLIC_ prefix
+export const clientSchema = z.object({
+	NEXT_PUBLIC_API_URL: z.string().url(),
+	NEXT_PUBLIC_WS_URL: z.string().url(),
+});
+
+export type ServerEnv = z.infer<typeof serverSchema>;
+export type EdgeEnv = z.infer<typeof edgeSchema>;
+export type ClientEnv = z.infer<typeof clientSchema>;
