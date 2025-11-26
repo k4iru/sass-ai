c326bd7b763c37843807a324d80b03516e9e47c2
Kyle Cheung <cheung.kyle1@gmail.com>
2025-11-26 15:46:02 -0500

    added alias for server files

--- server/server.ts
+++ server/server.ts
@@ -5,10 +5,10 @@ import { parse } from "node:url";
 import { config } from "dotenv";
 import { v4 as uuidv4 } from "uuid";
 import { WebSocket, WebSocketServer } from "ws";
+import { getJwtConfig } from "@/server/lib/jwtConfig";
 import { type AgentDeps, container } from "@/shared/lib/container";
 import { getChatModel } from "@/shared/lib/langchain/llmFactory";
 import { askQuestion } from "@/shared/lib/langchain/llmHandler";
-import { getJwtConfig } from "./lib/jwtConfig";
 
 type ChatRooms = {
 	[key: string]: WebSocket;
