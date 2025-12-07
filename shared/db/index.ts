73972ca031fca48ee77c3bbf52dc733a622266c4
Kyle Cheung <cheung.kyle1@gmail.com>
2025-12-07 17:41:52 -0500

    added ssl for railway

--- shared/db/index.ts
+++ shared/db/index.ts
@@ -1,19 +1,19 @@
-import { drizzle } from "drizzle-orm/node-postgres";
 import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-const { Pool, Client } = pg;
-import * as schema from "./schema";
+import * as schema from "@/shared/db/schema";
+
+const { Pool } = pg;
+const isProduction =
+	process.env.NODE_ENV === "production" ||
+	process.env.RUN_SEED_SCRIPT === "true";
 
 config({ path: ".env.local" });
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-// Create a dedicated client for LISTEN/NOTIFY
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL,
+	ssl: isProduction ? { rejectUnauthorized: false } : undefined,
 });
 
 export const db = drizzle({ client: pool }); // drizzle for updating / deleting / inserting data
-export { schema, client }; // client for LISTEN/NOTIFY
+export { schema }; // client for LISTEN/NOTIFY
