6b2d65653957b3138ac2296a5c3ad8000b2ba7e1
Kyle Cheung <cheung.kyle1@gmail.com>
2026-01-21 23:43:37 -0500

    reformatted document

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,12 @@
 "use client";
-import { useState } from "react";
 
 import clsx from "clsx";
-import styles from "./Sidebar.module.scss";
 import { ArrowLeftFromLine, ArrowRightFromLineIcon } from "lucide-react";
-
+import { useState } from "react";
 import { Input } from "@/components/ui/Input/Input";
-import { ChatListPanel } from "../ChatListPanel/ChatListPanel";
 import useChatMenuController from "@/hooks/useChatMenuController";
+import { ChatListPanel } from "../ChatListPanel/ChatListPanel";
+import styles from "./Sidebar.module.scss";
 
 export const Sidebar = () => {
 	const controller = useChatMenuController();
@@ -19,53 +18,51 @@ export const Sidebar = () => {
 	};
 
 	return (
-		<>
-			<aside className={clsx(styles.sidebar, isOpen && styles.open)}>
-				<div className={styles.topbar}>
-					<Input
-						type="text"
-						value={controller.searchTerm}
-						onChange={(e) => controller.setSearchTerm(e.target.value)}
+		<aside className={clsx(styles.sidebar, isOpen && styles.open)}>
+			<div className={styles.topbar}>
+				<Input
+					type="text"
+					value={controller.searchTerm}
+					onChange={(e) => controller.setSearchTerm(e.target.value)}
+				/>
+				<button
+					type="button"
+					className={styles.closeButton}
+					onClick={handleToggleSidebar}
+				>
+					<ArrowLeftFromLine
+						className={styles.closeIcon}
+						stroke="currentColor"
 					/>
-					<button
-						type="button"
-						className={styles.closeButton}
-						onClick={handleToggleSidebar}
-					>
-						<ArrowLeftFromLine
-							className={styles.closeIcon}
-							stroke="currentColor"
-						/>
-					</button>
-					<button
-						type="button"
-						className={styles.openButton}
-						onClick={handleToggleSidebar}
-					>
-						<ArrowRightFromLineIcon
-							className={styles.openIcon}
-							stroke="currentColor"
-						/>
-					</button>
-				</div>
-				{controller.filteredChats && controller.filteredChats.length > 0 ? (
-					<ChatListPanel
-						filteredChats={controller.filteredChats}
-						openMenuId={controller.openMenuId}
-						setOpenMenuId={controller.setOpenMenuId}
-						pendingDeleteId={controller.pendingDeleteId}
-						setPendingDeleteId={controller.setPendingDeleteId}
-						renamingChatId={controller.renamingChatId}
-						setRenamingChatId={controller.setRenamingChatId}
-						newTitle={controller.newTitle}
-						setNewTitle={controller.setNewTitle}
-						handleRenameSubmit={controller.handleRenameSubmit}
-						handleDelete={controller.handleDelete}
+				</button>
+				<button
+					type="button"
+					className={styles.openButton}
+					onClick={handleToggleSidebar}
+				>
+					<ArrowRightFromLineIcon
+						className={styles.openIcon}
+						stroke="currentColor"
 					/>
-				) : (
-					<></>
-				)}
-			</aside>
-		</>
+				</button>
+			</div>
+			{controller.filteredChats && controller.filteredChats.length > 0 ? (
+				<ChatListPanel
+					filteredChats={controller.filteredChats}
+					openMenuId={controller.openMenuId}
+					setOpenMenuId={controller.setOpenMenuId}
+					pendingDeleteId={controller.pendingDeleteId}
+					setPendingDeleteId={controller.setPendingDeleteId}
+					renamingChatId={controller.renamingChatId}
+					setRenamingChatId={controller.setRenamingChatId}
+					newTitle={controller.newTitle}
+					setNewTitle={controller.setNewTitle}
+					handleRenameSubmit={controller.handleRenameSubmit}
+					handleDelete={controller.handleDelete}
+				/>
+			) : (
+				<div></div>
+			)}
+		</aside>
 	);
 };
