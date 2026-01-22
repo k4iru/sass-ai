7f1e2b5b550db51e840f20bda4feb8b6636235c7
Kyle Cheung <cheung.kyle1@gmail.com>
2026-01-22 01:14:09 -0500

    fixed colon bug

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+// eslint.config.mjs
+
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,8 +13,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
 	...compat.extends("next/core-web-vitals", "next/typescript"),
-	rules,
-	{ "@typescript-eslint/no-unused-vars": "off" },
+	{
+		rules: {
+			"@typescript-eslint/no-unused-vars": "off",
+			"@typescript-eslint/no-explicit-any": "off", // Helpful for AI projects
+		},
+	},
 ];
 
 export default eslintConfig;
