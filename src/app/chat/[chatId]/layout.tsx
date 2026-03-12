ecc4444eabfd6911879a7cd9d682c4142e03c47a
Kyle Cheung <cheung.kyle1@gmail.com>
2026-03-12 19:35:35 -0400

    removed unused imports

--- src/app/chat/[chatId]/layout.tsx
+++ src/app/chat/[chatId]/layout.tsx
@@ -1,7 +1,3 @@
-import { use } from "react";
-import { useChat } from "@/context/ChatContext";
-import styles from "./layout.module.scss";
-
 const chatIdLayout = ({ children }: { children: React.ReactNode }) => {
 	return <>{children}</>;
 };
